a2bbc6eb41f9f43d5e728516f45e7ec4d988165b
zhengyu <zhengyu159@126.com>
2018-06-06 14:52:42 +0800

    bug分支

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             System.out.println(".....");
             System.out.println("111111111111111111111111");
+            System.out.println("222222222222222222222222");
         }
     }
 }
