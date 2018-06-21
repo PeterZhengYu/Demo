3166849b293d017e91b73d06e963ed37828462dc
zhengyu <zhengyu159@126.com>
2018-06-21 16:35:42 +0800

    测试

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -5,66 +5,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button bt_button;
+    @Inject
+    private Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt_button = findViewById(R.id.bt_button);
 
-        bt_button.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                MyThread thread1 = new MyThread("窗口1");
-                MyThread2 thread2 = new MyThread2("窗口2");
-                thread1.start();
-                thread2.start();
-            }
-        });
     }
 
-    public class MyThread extends Thread {
-        private int count = 100;
-        private String name;
 
-        public MyThread(String name) {
-            this.name = name;
-        }
-        @Override
-        public void run() {
-            while (count > 0) {
-                count--;
-                System.out.println(name + "卖掉了1张票，剩余票数为：" + count);
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public class MyThread2 extends Thread {
-        private int count = 100;
-        private String name;
-
-        public MyThread2(String name) {
-            this.name = name;
-        }
-        @Override
-        public void run() {
-            System.out.println(".....");
-            System.out.println("111111111111111111111111");
-            System.out.println("222222222222222222222222");
-            System.out.println("333333333333333333333333");
-            System.out.println("444444444444444444444444");
-            System.out.println("555555555555555555555555");
-            System.out.println("6666666666666666666666666");
-        }
-    }
 }
---
+++ app/src/main/java/com/example/demo/Person.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Person {
+    public Person() {
+        System.out.println("a person created");
+    }
+}
