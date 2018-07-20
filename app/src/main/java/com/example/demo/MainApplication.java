7aa505b381adb7153320bb68c1c231f0bb13e3bd
Peter <zhengyu159@126.com>
2018-07-20 11:18:39 +0800

    ...

--- app/src/main/java/com/example/demo/MainApplication.java
+++ app/src/main/java/com/example/demo/MainApplication.java
@@ -7,5 +7,6 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        System.out.println("111111111111111111111111111");
     }
 }
